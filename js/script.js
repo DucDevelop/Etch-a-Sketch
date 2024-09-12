db4c27784f7ec0034fcdc5d7475572772a67ca3d
Duc <145208899+DucDevelop@users.noreply.github.com>
2024-09-12 15:16:17 +0200

    Randomize color of visited fields

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ function createGrid(n,container) {
     // calculate width of single element
     let containerWidth = window.getComputedStyle(containerRef, null).getPropertyValue('width');
     let containerPad = window.getComputedStyle(containerRef, null).getPropertyValue('padding');
-    
+
     const width = 1 / n;
-    const height =  Math.floor((+containerWidth.slice(0, containerWidth.length-2) - 2* +containerPad.slice(0, containerPad.length-2)) * width );
+    const height =  (+containerWidth.slice(0, containerWidth.length-2) - 2* +containerPad.slice(0, containerPad.length-2)) * width ;
 
     for(let i = 0; i < n*n; ++i) {
         const square = document.createElement('div');
@@ -23,8 +23,9 @@ function createGrid(n,container) {
 
 containerRef.addEventListener('mouseover', (e) => {
     if(e.target.classList.contains('grid-element'))
-        e.target.classList.add('visited-element');
+        e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 })
+
 btnRef.addEventListener('click', () => {
     const N = prompt('Enter N to create a NxN grid (maximum 100)');
     if(N > 0 && N <=100) {
@@ -34,6 +35,5 @@ btnRef.addEventListener('click', () => {
           createGrid(N, containerRef)
     }
 })
-
 
 createGrid(16, containerRef)
